6995471884c1ef5eea802eeea48f22d5f7e7e70c
Thiago-Levi <thiago.levi@outlook.com>
2023-12-12 12:04:21 -0300

    :sparkles: feat: Implementei outra validação da rota cadastrar pedido

--- src/controladores/pedido/pedidoControlador.cadastrar.js
+++ src/controladores/pedido/pedidoControlador.cadastrar.js
@@ -1,7 +1,33 @@
+const knex = require("../../bancodedados/conexao");
 const cadastrarPedido = async (req, res) => {
   const { cliente_id, observacao, pedido_produtos } = req.body;
 
   try {
+    const existeCliente_id = await knex("clientes")
+      .where("id", "=", cliente_id)
+      .first();
+
+    if (!existeCliente_id) {
+      return res.status(401).json("cliente_id inválido.");
+    }
+
+    const produtosDoPedido = await Promise.all(
+      pedido_produtos.map(async (pedido_produto) => {
+        return await knex("produtos")
+          .where("id", "=", pedido_produto.produto_id)
+          .first("*");
+      })
+    );
+
+    const algumProduto_idInexistente = produtosDoPedido.some(
+      (produtoDoPedido) => produtoDoPedido === undefined
+    );
+
+    if (algumProduto_idInexistente) {
+      return res.status(404).json("Existe algum produto_id inválido.");
+    }
+
+    //Validar se existe a quantidade em estoque de cada produto existente dentro do array, de acordo com a quantidade informada no corpo (body) da requisição.
     return res.status(200).json();
   } catch (error) {
     return res.status(404).json(error.message);
