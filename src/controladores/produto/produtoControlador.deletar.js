ec390ca04edb527c0142ae33dff867d947d3438e
Thiago-Levi <thiago.levi@outlook.com>
2023-12-18 10:26:10 -0300

    validação em deletar produto

--- src/controladores/produto/produtoControlador.deletar.js
+++ src/controladores/produto/produtoControlador.deletar.js
@@ -4,6 +4,18 @@ const deletarProduto = async (req, res) => {
   const { id } = req.params;
 
   try {
+    const existeProdutoVinculadoAAlgumPedido = await knex("pedido_produtos")
+      .where("produto_id", "=", id)
+      .select("*");
+
+    if (existeProdutoVinculadoAAlgumPedido.length != 0) {
+      return res
+        .status(404)
+        .json(
+          "Este produto está vinculado a pedidos, por tanto, não pode ser excluído."
+        );
+    }
+
     if (id) {
       produto = await knex("produtos").del().where({ id }).returning();
     }
@@ -14,6 +26,7 @@ const deletarProduto = async (req, res) => {
 
     return res.status(200).json("Produto deletado com sucesso");
   } catch (error) {
+    console.log(error.message);
     return res.status(404).json("id do produto inválido");
   }
 };
