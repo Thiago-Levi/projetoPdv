849a70d945ed3c534d011f8985f10cdc1ce7c86f
Thiago-Levi <thiago.levi@outlook.com>
2023-12-25 13:39:28 -0300

    :sparkles: feat: config cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const routes = require("./rotas/rotas");
 const port = process.env.SERVER_PORT;
+app.use(cors());
+
 app.use(express.json());
 app.use(routes);
 app.listen(port, console.log(`Servidor rodando na porta ${port}`));
