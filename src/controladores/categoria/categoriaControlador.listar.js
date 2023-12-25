7bb1191ae557ec43b8353b901388e58dbad32c18
Thiago-Levi <thiago.levi@outlook.com>
2023-12-25 14:36:52 -0300

    :sparkles: feat: config json

--- src/controladores/categoria/categoriaControlador.listar.js
+++ src/controladores/categoria/categoriaControlador.listar.js
@@ -1,9 +1,9 @@
 const knex = require("../../bancodedados/conexao");
 
-const listarCategorias = async (_, res) => {
+const listarCategorias = async (req, res) => {
   try {
     const categorias = await knex("categorias").select("*");
-    return res.status(200).json(categorias);
+    return res.status(200).json({ categorias });
   } catch (error) {
     return res.status(404).json({ mensagem: "Erro interno no servidor" });
   }
